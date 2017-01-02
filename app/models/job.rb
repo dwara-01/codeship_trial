f1f93fb1775fb00fb7949df6ce67f648799b2ccb
dwarakgovind <dwarakgovindp@gmail.com>
2017-01-02 15:35:13 +0530

    Updated Job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,20 @@ class Job < ActiveRecord::Base
 
   belongs_to :user
   #this is a job
+
+  def new_job
+      j = self.class.new
+      j.save
+  end
+
+  def send_offer
+      u = User.find(j.id)
+      send_mail(u, :offer)
+  end
+
+  def send_mail(a,b)
+      # to do
+  end
+
+
 end
